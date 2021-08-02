03e2d8d9c815ec8c8d2df6034021d26aa91192df
BASSAM <sold@outlook.com>
2021-08-02 10:30:22 +0300

    OAuth2.0 with spring boot

---
+++ src/main/java/me/bsa10/OAuth/Controller/indexController.java
@@ -0,0 +1,19 @@
+package me.bsa10.OAuth.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class indexController {
+
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+
+}
---
+++ src/test/java/me/bsa10/OAuth/OAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bsa10.OAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
